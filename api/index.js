5e9d34afbf0416c1a92f932ec077fc5a21b277a6
Z Zhong <zhongzhyu@gmail.com>
2025-01-08 21:56:17 +0800

    market api

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import fs from 'fs';
 
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
   return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
 })
+
+app.post('/upload_market_book', async (c) => {
+  const data = await c.req.json();
+  fs.writeFileSync('milkyapi.json', JSON.stringify(data, null, 2));
+  return c.json({ message: 'Data saved successfully' });
+});
 
 const handler = handle(app);
 
